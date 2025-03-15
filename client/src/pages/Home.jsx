6007c2b3374faa78b42effc90eadf526b1d09e0f
Nish Patel <nish14062003@gmail.com>
2025-03-15 11:46:47 +0530

    Description added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Steps from "../components/Steps";
+import Description from "../components/Description";
 
 const Home = () => {
   return <div>
           <Hero />
           <Steps />
+          <Description />
   
           </div>;
 };
