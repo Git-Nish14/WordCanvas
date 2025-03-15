8ec6e2e4bcc3c897ca895887dfa84015f61e192a
Nish Patel <nish14062003@gmail.com>
2025-03-15 11:32:17 +0530

    steps added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Steps from "../components/Steps";
 
 const Home = () => {
-  return <div><Hero /></div>;
+  return <div>
+          <Hero />
+          <Steps />
+  
+          </div>;
 };
 
 export default Home;
